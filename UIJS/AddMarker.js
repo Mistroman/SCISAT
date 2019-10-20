2284d84bb4d286f8740dba684456116e593ef781
ManZi <manzi@ubuntu.ubuntu-domain>
2019-10-19 18:13:59 -0600

    Adding Marker capability added

--- UIJS/AddMarker.js
+++ UIJS/AddMarker.js
@@ -1,5 +1,7 @@
 function NewMarker(lat, long, markerMess){
-    var marker = WE.marker([lat, long]).addTo(earth)
-    marker.bindPopup('<b>Hello world!</b>');
+    var marker = WE.marker([lat, long]).addTo(earth);
+        
+        marker.bindPopup("<b><span id='myText'></span></b>", {maxWidth: 150, closeButton: true}).openPopup();
+        document.getElementById("myText").innerHTML = markerMess;
 
 }
--- UIJS/index.js
+++ UIJS/index.js
@@ -1,11 +1,14 @@
-
-
+var options = {atmosphere: true, center: [0, 0], zoom: 0};
+var earth = new WE.map('map', options);
 function initialize() {
-  var options = {atmosphere: true, center: [0, 0], zoom: 0};
-  var earth = new WE.map('map', options);
-  WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+  
+  
+  WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           minZoom: 0,
           maxZoom: 5,
           attribution: 'NASA'
         }).addTo(earth);
+
+
+        
       }
