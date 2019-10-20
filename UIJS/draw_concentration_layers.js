61f58203b1acd2484c62e6a2c9dfc1b6ed1794c1
ManZi <manzi@ubuntu.ubuntu-domain>
2019-10-20 03:58:32 -0600

    Added mapping of ozoneconcentration to applicable color densities Added mapping of altitudes to applicable radius of the circle

---
+++ UIJS/draw_concentration_layers.js
@@ -0,0 +1,23 @@
+function drawConcentratonLayers(altitudes, ozoneConcentrations){
+    // center should always be x: 102, y: 182
+    
+    var mapRange = function(from, to, s) {
+      return to[0] + (s - from[0]) * (to[1] - to[0]) / (from[1] - from[0]);
+    }
+    
+
+    for(i = 0; i<altitudes.length; i++){
+        let color_string;
+        var r = mapRange([10,0], [0,255], ozoneConcentrations[i]);
+        var radius = mapRange([5, 95], [102, 182], altitudes[i]);
+        rs = r.toString(10);
+        console.log(r);
+        color_string  = "rgb(255," + rs +", "+ rs +")";
+        console.log(color_string);
+        drawCircle(102, 182, radius, 0, 2*Math.PI, color_string);
+      }
+
+      
+      
+
+  }
